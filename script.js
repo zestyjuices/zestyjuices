746c36cb21e903d736d5f65ab786dda16cf453d7
zestyjuices <zestyjuiceco@gmail.com>
2025-03-01 17:32:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ document.addEventListener("DOMContentLoaded", () => {
       });
   });
 
+  document.querySelectorAll('.card a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        document.getElementById(targetId)?.scrollIntoView({ behavior: "smooth" });
+    });
+});
+
   // Fix for button smooth scrolling
   document.getElementById("explore-menu-btn").addEventListener("click", () => {
     document.getElementById("menu").scrollIntoView({ behavior: "smooth" });
